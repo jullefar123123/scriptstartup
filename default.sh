3b6f0f46862b0f39b6aaa17f57b63aabfd0fef5f
jullefar123123 <jesper.c123@gmail.com>
2025-06-14 12:39:17 +0200

    Update default.sh

--- default.sh
+++ default.sh
@@ -1,24 +1,8 @@
-[root@nodo01 ~]# chmod 700 /tmp/config_hugepages80.sh
-
-[root@nodo01 ~]# cat /tmp/config_hugepages80.sh
-#!/bin/bash
-
-round() {
-# Función para sacar la media de las cpus (de la función mira_cpus) ya que en algunas bash no funciona el típico:
-# echo "(`lscpu |grep ^"CPU(s):" | awk -F: {'print $2'} | tr -d '[:blank:]'`)/2" | bc
-echo $(printf %.$2f $(echo "scale=$2;(((10^$2)*$1)+0.5)/(10^$2)" | bc))
-}
-
-memlibre=`awk '/Hugepagesize:/{p=$2}/ 0 /{next}/ kB$/{v[sprintf("%9d GB %-s",int($2/1024/1024),$0)]=$0;next}{h[$0]=$2}END{for(k in v) print k;for (k in h) print sprintf("%9d GB %-s",p*h[k]/1024/1024,k)}' /proc/meminfo|sort -nr|grep --color=auto -E "^|.(Huge._[TF]|Mem).*:|" |grep MemFree | awk '{ print $4 }'`
-
-pagesize=`awk '/Hugepagesize:/{p=$2}/ 0 /{next}/ kB$/{v[sprintf("%9d GB %-s",int($2/1024/1024),$0)]=$0;next}{h[$0]=$2}END{for(k in v) print k;for (k in h) print sprintf("%9d GB %-s",p*h[k]/1024/1024,k)}' /proc/meminfo|sort -nr|grep --color=auto -E "^|.(Huge._[TF]|Mem).*:|" |grep Hugepagesize | awk '{ print $4 }'`
-
-hps=`echo $(round ${memlibre}/${pagesize} 0)`
-pags=`echo $(round ${hps}*80/100 0)`
-
-echo "${pags}" > /proc/sys/vm/nr_hugepages
-echo "vm.nr_hugepages = ${pags}" >> /etc/sysctl.conf
-
-sysctl -p >>/dev/null
-
-awk '/Hugepagesize:/{p=$2}/ 0 /{next}/ kB$/{v[sprintf("%9d GB %-s",int($2/1024/1024),$0)]=$0;next}{h[$0]=$2}END{for(k in v) print k;for (k in h) print sprintf("%9d GB %-s",p*h[k]/1024/1024,k)}' /proc/meminfo|sort -nr|grep --color=auto -E "^|.(Huge._[TF]|Mem).*:|"
+sudo mkdir -p /etc/kernel/cmdline.d
+cat << EOF | sudo tee -a /etc/kernel/cmdline.d/hugepages.conf
+default_hugepagesz=1G
+hugepagesz=1G
+hugepages=192
+EOF
+sudo clr-boot-manager update
+sudo reboot
